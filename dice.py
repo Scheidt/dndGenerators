64f1a7bce769564dd2b2f7bb8be9d465bba53e05
Pedro Scheidt <pedrohenriqueprazeres@hotmail.com>
2023-08-15 19:19:54 -0300

    thought of a less stupid way of doing things

--- dice.py
+++ dice.py
@@ -3,38 +3,11 @@ from random import randint
 def genericDie(faces):
     return randint(1, faces)
 
-# This may look stupid programming-wise, but in the game different dice are refered as 'x'd'y', where
-# 'x' is the amount of dice to be rolled
-# 'y' is the amount of faces those dices have
-# so if I write 3d12, I'm rolling 3 twelve-faced dice
-# writing the code this way makes it so it is closer to the game.
-def d2():
-    return genericDie(2)
-
-def d4():
-    return genericDie(4)
-
-
-def d6():
-    return genericDie(6)
-
-
-def d8():
-    return genericDie(8)
-
-
-def d10():
-    return genericDie(10)
-
-
-def d12():
-    return genericDie(12)
-
-
-def d20():
-    return genericDie(20)
-
-
-def d100():
-    return genericDie(100)
-
+def roll(diceCode: str):
+    ammount = int(diceCode[0])
+    numOfFaces = int(diceCode[2, -1])
+    result = []
+    while ammount <= 1:
+        result.append(genericDie(numOfFaces))
+        ammount -= 1
+    return result
